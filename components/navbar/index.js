c7269b55db7c2d69a2cbc68fa697b78b84823661
vidyesh-kumar <kumarvidyesh@gmail.com>
2022-09-21 22:10:38 +0530

    Minor Fixes

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,7 +1,6 @@
 import { navLinks } from '../constants/index'
 import Link from 'next/link'
 import Image from 'next/image'
-import Image from 'next/image'
 
 export default function Navbar() {
   return (
